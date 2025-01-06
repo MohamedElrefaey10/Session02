f0cc12955db387a356e80baa36f42003882492bc
MoGa <MoGa@DESKTOP-HSMA4DI>
2025-01-06 22:10:30 +0200

    video01

--- Session02/Program.cs
+++ Session02/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            
+            #region intro To OOP
+
+            // 1. Encapsulation
+            // 2. inheritance
+            // 3. polymorhism
+            // 4. Abstraion
+
+            #endregion
         }
     }
 }
