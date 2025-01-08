a286c1bb9dff27029d4a172644d5002c5d5e0dd7
MoGa <MoGa@DESKTOP-HSMA4DI>
2025-01-08 02:59:42 +0200

    video02

---
+++ Session02/Encapsulation/Employee.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session02.Encapsulation
+{
+    internal struct Employee
+    {
+        #region Attributes
+        public int Id;
+        private string? Name;
+        private decimal salary;
+        
+        #endregion
+
+        #region Constructors
+        public Employee(int id, string name, decimal _salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = _salary;
+        }
+        #endregion
+
+        #region Methods
+        public override string ToString()
+        {
+            return $"Id : {Id}\nName : {Name}\nSalary : {salary:c}";
+        }
+        #endregion
+        #region Getter - setter
+
+        public string? GetName()
+        {
+            return Name;
+        }
+        public void SetName(string? name)
+        {
+            this.Name = name?.Length > 5?name.Substring(0,5):name;
+        }
+
+        #endregion
+
+        #region Propirty
+
+        public decimal Salary
+        {
+            get 
+        {
+                return salary;
+            }
+            set
+            {
+                salary = value > 5000 ? 5000 : value;
+            }
+        }
+
+        //private int age;
+
+        //public int Age
+        //{
+        //    get
+        //    { return age; }
+        //    set
+        //    { age = value; }
+        //}
+
+        public int Age { get; set; }
+
+        //private decimal deduction;
+
+        public decimal Deduction
+        {
+            get { return Salary * 0.2m; }
+        }
+
+        #endregion
+    }
+    }
--- Session02/Program.cs
+++ Session02/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Session02
+﻿using Session02.Encapsulation;
+
+namespace Session02
 {
     internal class Program
     {
@@ -11,6 +13,27 @@
             // 3. polymorhism
             // 4. Abstraion
 
+            #endregion
+
+            #region Encapsulation
+            //Employee employee1 = new Employee();
+            //Console.WriteLine(employee1);
+            //Console.WriteLine(employee1.ToString());
+
+            Employee employee = new Employee(70, "Ali", 4000);
+
+            Console.WriteLine(employee);
+            //employee.Id = 100;
+
+            //Console.WriteLine(employee.Id);
+            //employee.SetName("Mohammed");
+            //Console.WriteLine(employee.GetName());
+
+            employee.Salary = 6000;
+            Console.WriteLine(employee.Salary);
+            employee.Age = 20;
+            Console.WriteLine(employee.Age);
+            Console.WriteLine(employee.Deduction);
             #endregion
         }
     }
